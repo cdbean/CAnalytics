cef0cff904cdb8f7f934a55eed03f9911cf70d8f
Dong Chen <imdongchen@gmail.com>
2015-10-28 14:42:27 -0400

    add user name in log export

--- sync/admin.py
+++ sync/admin.py
@@ -21,6 +21,7 @@ def export_csv(modeladmin, request, queryset):
         smart_str(u"Group Name"),
         smart_str(u"Timestamp"),
         smart_str(u"Sender ID"),
+        smart_str(u"Sender Username"),
         smart_str(u"Sender Name"),
         smart_str(u"Content"),
     ])
@@ -34,6 +35,7 @@ def export_csv(modeladmin, request, queryset):
 			    smart_str(obj.group.name),
 			    smart_str(obj.sent_at.strftime('%m/%d/%Y-%H:%M:%S')),
 			    smart_str(obj.sender.id),
+                smart_str(obj.sender.first_name + ' ' + obj.sender.last_name),
 			    smart_str(obj.sender.username),
 			    smart_str(obj.content),
 			])
