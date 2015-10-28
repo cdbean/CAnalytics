f57233285f46470969dc01dc0f4e6dfbe37084b0
Dong Chen <imdongchen@gmail.com>
2015-10-28 11:16:12 -0400

    bug fix

--- logger/admin.py
+++ logger/admin.py
@@ -51,7 +51,7 @@ def log_export_csv(modeladmin, request, queryset):
 
 def entity_log_export_csv(modeladmin, request, queryset):
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=logs.csv'
+    response['Content-Disposition'] = 'attachment; filename=entity_logs.csv'
     writer = csv.writer(response, csv.excel)
     # response.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
     writer.writerow([
@@ -96,7 +96,7 @@ def entity_log_export_csv(modeladmin, request, queryset):
 
 def rel_log_export_csv(modeladmin, request, queryset):
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=logs.csv'
+    response['Content-Disposition'] = 'attachment; filename=rel_logs.csv'
     writer = csv.writer(response, csv.excel)
     # response.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
     writer.writerow([
