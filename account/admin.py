c1ccc9db9de4e52d068564f9942c80d968c1cfc3
Dong Chen <imdongchen@gmail.com>
2015-11-02 04:52:59 -0500

    show user full in admin

--- account/admin.py
+++ account/admin.py
@@ -43,6 +43,11 @@ export_csv.short_description = u"Export CSV"
 class MyModelAdmin(admin.ModelAdmin):
     actions = [export_csv,]
 
+def user_unicode(self):
+    return  u'%s, %s' % (self.last_name, self.first_name)
+
+User.__unicode__ = user_unicode
+
 admin.site.unregister(User)
 admin.site.register(User, MyModelAdmin)
 
